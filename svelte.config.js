9de0a77c56ba37c21c6fb0ee9d47a9c50fe1e024
Alireza Modiriyan <alireza.modiriyan98@gmail.com>
2025-04-25 15:50:33 +0100

    changed config

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: 'index.html', // Important for routing to work on GitHub Pages
+    }),
     paths: {
-      base: '/berrys',  // This matches your GitHub Pages path
+      base: '/berrys', // This matches your GitHub Pages path
     },
   },
 };
